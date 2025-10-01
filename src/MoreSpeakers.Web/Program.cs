ef563b693cd864f22b252cba42a5b0f23d525856
Joseph Guadagno <jguadagno@hotmail.com>
2025-09-30 20:58:05 -0700

    Added ApplicationInsights and SeriLog

--- src/MoreSpeakers.Web/Program.cs
+++ src/MoreSpeakers.Web/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.WindowsServer;
 using Microsoft.AspNetCore.Identity;
 using morespeakers.Data;
 using morespeakers.Models;
 using morespeakers.Services;
+using NuGet.Configuration;
+using Serilog;
+using Serilog.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Aspire service defaults
 builder.AddServiceDefaults();
+
+// Add logging and telemetry
+builder.Services.AddSingleton<ITelemetryInitializer, AzureWebAppRoleEnvironmentTelemetryInitializer>();
+builder.Services.AddApplicationInsightsTelemetry();
+
+// Configure the logger
+var fullyQualifiedLogFile = Path.Combine(builder.Environment.ContentRootPath, "logs\\logs.txt");
+ConfigureLogging(builder.Configuration, builder.Services, fullyQualifiedLogFile, "Web");
 
 // Add database context
 builder.AddSqlServerDbContext<ApplicationDbContext>("sqldb");
@@ -95,3 +108,30 @@ app.UseSession();
 app.MapRazorPages();
 
 app.Run();
+
+void ConfigureLogging(IConfigurationRoot configurationRoot, IServiceCollection services, string logPath, string applicationName)
+{
+    var logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithMachineName()
+        .Enrich.WithThreadId()
+        .Enrich.WithEnvironmentName()
+        .Enrich.WithAssemblyName()
+        .Enrich.WithAssemblyVersion(true)
+        .Enrich.WithExceptionDetails()
+        .Enrich.WithProperty("Application", applicationName)
+        .Destructure.ToMaximumDepth(4)
+        .Destructure.ToMaximumStringLength(100)
+        .Destructure.ToMaximumCollectionCount(10)
+        .WriteTo.Console()
+        .WriteTo.File(logPath, rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+    services.AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.AddApplicationInsights(configureTelemetryConfiguration: (config) =>
+                config.ConnectionString =
+                    configurationRoot["APPLICATIONINSIGHTS_CONNECTION_STRING"],
+            configureApplicationInsightsLoggerOptions: (_) => { });loggingBuilder.AddApplicationInsights();
+        loggingBuilder.AddSerilog(logger);
+    });
+}
