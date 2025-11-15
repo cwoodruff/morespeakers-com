084203a4c817b3526688f461c288f4c729fc43ac
Joseph Guadagno <jguadagno@hotmail.com>
2025-11-14 22:53:53 -0700

    Added exception handling and logging to MentoringDataStore.cs

--- src/MoreSpeakers.Data/MoreSpeakersDbContext.cs
+++ src/MoreSpeakers.Data/MoreSpeakersDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.Intrinsics.Arm;
-
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
