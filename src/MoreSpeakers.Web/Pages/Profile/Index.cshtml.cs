338ce78f59fac6ce877ca5771dce271e5a51beff
Joseph Guadagno <jguadagno@hotmail.com>
2025-11-19 14:12:49 -0700

    Refactored the Profile View components so that we were not duplicating them. Issue #178

--- src/MoreSpeakers.Web/Pages/Profile/Index.cshtml.cs
+++ src/MoreSpeakers.Web/Pages/Profile/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,38 +6,70 @@ using MoreSpeakers.Domain.Models;
 
 namespace MoreSpeakers.Web.Pages.Profile;
 
-[Authorize]
 public class IndexModel(IUserManager userManager, ILogger<IndexModel> logger) : PageModel
 {
 
-    public User ProfileUser { get; set; } = null!;
-    public IEnumerable<UserExpertise> UserExpertise { get; set; } = new List<UserExpertise>();
+    public User UserProfile { get; set; } = null!;
     public bool CanEdit { get; set; }
+    
+    [BindProperty(SupportsGet = true)]
+    public Guid? Id { get; set; } = null;
 
     public async Task<IActionResult> OnGetAsync()
     {
+        // There are two possible views for this page
+        // - A person viewing another persons profile. (default)
+        //    This requires just an Id query parameter
+        // - A logged in user that is visiting their profile
+        //    This happens if a person to be signed in and no Id query parameter passed
+        //    This happens if a person to be signed in and their Id passed
+        
         User? identityUser = null;
+
         try
         {
             identityUser = await userManager.GetUserAsync(User);
-            if (identityUser == null)
-            {
-                return Challenge();
-            }
 
-            // Load profile with all the user's data
-            var userProfile = await userManager.GetAsync(identityUser.Id);
-            if (userProfile == null)
+            User? userProfile;
+            if (Id.HasValue && Id.Value != Guid.Empty)
             {
-                logger.LogError("Error loading profile page. Could not find user. UserId: '{UserId}'", identityUser?.Id);
-                return RedirectToPage("/Profile/LoadingProblem",
-                    new { UserId = identityUser?.Id ?? Guid.Empty });
-            }
-            
-            ProfileUser = userProfile;
-            UserExpertise = await userManager.GetUserExpertisesForUserAsync(identityUser.Id);
-            CanEdit = true; // User can always edit their own profile
+                userProfile = await userManager.GetAsync(Id.Value);    
+                if (userProfile == null)
+                {
+                    logger.LogError("Error loading profile page. Could not find user. UserId: '{UserId}'", identityUser?.Id);
+                    return RedirectToPage("/Profile/LoadingProblem",
+                        new { UserId = Id});
+                }
 
+                if (userProfile.Id == identityUser?.Id)
+                {
+                    CanEdit = true;
+                }
+                UserProfile = userProfile;    
+            }
+            else
+            {
+                if (identityUser is not null)
+                {
+                    userProfile = await userManager.GetAsync(identityUser.Id);    
+                    if (userProfile == null)
+                    {
+                        logger.LogError("Error loading profile page. Could not find user. UserId: '{UserId}'", identityUser.Id);
+                        return RedirectToPage("/Profile/LoadingProblem",
+                            new { UserId = identityUser.Id});
+                    }
+                    UserProfile = userProfile; 
+                    if (userProfile.Id == identityUser?.Id)
+                    {
+                        CanEdit = true;
+                    }
+                }
+                else
+                {
+                    return RedirectToPage("/Profile/LoadingProblem",
+                        new { UserId = Guid.Empty });
+                }
+            }
         }
         catch (Exception ex)
         {
