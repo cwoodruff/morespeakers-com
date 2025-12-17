0e654beb8a3d5b83af41f3bdbe2b98a96ec51ca9
Joseph Guadagno <jguadagno@hotmail.com>
2025-12-17 11:09:18 -0700

    Added the missing IsActive property to Expertise model and data context

--- src/MoreSpeakers.Domain/Models/Expertise.cs
+++ src/MoreSpeakers.Domain/Models/Expertise.cs
@@ -11,6 +11,8 @@ public class Expertise
     [MaxLength(500)] public string? Description { get; set; }
 
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    
+    public bool IsActive { get; set; } = true;
 
     // Foreign keys
     public int ExpertiseCategoryId { get; set; }
